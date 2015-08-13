5516d50a34e98c126124c7397a2397c636883b24
Patrick Nguyen <Patrick.Nguyen@mindgeek.com>
2015-08-13 18:11:35 -0400

    Example of Accessing the Session Object

--- app/controllers/Contacts.java
+++ app/controllers/Contacts.java
@@ -13,7 +13,20 @@ public class Contacts extends Controller {
 
 	public static Result index() {
 		List<String> names = Arrays.asList("James", "Emma", "Ollie", "Nate");
-		return ok(index.render("Hello, once again, World", names));
+		return ok(index.render("Hello, once again, World... <small>with feeling</small>", names));
+	}
+
+	public static Result another() {
+		if(!session().containsKey("counter")) {
+			session("counter", "0");
+		}
+
+		String currentStringValue = session("counter");
+		Integer currentValue = Integer.parseInt(currentStringValue);
+		Integer newValue = currentValue + 1;
+		session("counter", newValue.toString());
+
+		return ok(another.render());
 	}
 
 	public static Result create() {
@@ -31,4 +44,5 @@ public class Contacts extends Controller {
 	public static Result delete(Long id) {
 		return TODO;
 	}
+
 }
